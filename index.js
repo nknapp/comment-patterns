ea5e501e3fdef38c128d8ffa10960f6b09759005
Nils Knappmeier <n.knappmeier@i-views.de>
2015-05-01 21:40:09 +0200

    Use verb for creating the README, added apidoc-comment

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var path = require("path");
 var db = require("./lang-db/lang.js");
 var _ = require("lodash");
 
-module.exports.byFileName = function (filename) {
+/**
+ * Load the comment-pattern for a given file.
+ * The file-language is determined by the file-extension.
+ * @param {string} filename the name of the file
+ * @returns {object} the comment-patterns
+ * @api public
+ */
+module.exports = function (filename) {
     var ext = path.extname(filename);
     return _.find(db, function (item) {
         return _.any(item.nameMatchers, function (matcher) {
